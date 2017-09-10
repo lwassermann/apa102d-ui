800b68c55e7006a0e63c449c5d8c2b4e5b7b1c57
Lars Wassermann <lars.wassermann@googlemail.com>
2017-09-10 16:41:04 +0200

    Show the current value

--- src/parameters/color.jsx
+++ src/parameters/color.jsx
@@ -9,7 +9,7 @@ const SATURATIONS = 16
 const VALUES = 32
 const RATIO = 0.9
 
-function fillHSV(ctx, x, y, width, height) {
+function fillHS(ctx, x, y, width, height) {
   for (let hue = 0; hue < HUES; hue += 1) {
     for (let saturation = 0; saturation < SATURATIONS; saturation += 1) {
       const { r, g, b } = HSVtoRGB(hue / HUES, 1 - saturation / SATURATIONS, 1)
@@ -24,11 +24,24 @@ function fillHSV(ctx, x, y, width, height) {
   }
 }
 
-function fillValue(ctx, x, y, width, height) {
+function calculateHS(x, y, width, height) {
+  return {
+    h: x / width,
+    s: (height - y) / height
+  }
+}
+
+function fillV(ctx, x, y, width, height) {
   for (let value = 0; value < VALUES; value += 1) {
     const { r, g, b } = HSVtoRGB(0, 0, value / VALUES)
     ctx.fillStyle = `rgb(${r},${g},${b})`
     ctx.fillRect(x + value * width / VALUES, y, width / VALUES + 1, height)
+  }
+}
+
+function calculateValueOf(x, y, width, height) {
+  return {
+    v: x / width
   }
 }
 
@@ -45,11 +58,11 @@ class Color extends React.Component {
       const ctx = canvas.getContext('2d')
       const { width, height } = canvas
 
-      fillHSV(ctx, 0, 0, width * RATIO, height)
+      fillHS(ctx, 0, 0, width * RATIO, height)
 
       ctx.transform(1, 0, 0, 1, width, 0)
       ctx.rotate(Math.PI / 2)
-      fillValue(ctx, 0, 0, height, width * (1 - RATIO))
+      fillV(ctx, 0, 0, height, width * (1 - RATIO))
     }
 
     this.canvas = canvas
@@ -58,25 +71,81 @@ class Color extends React.Component {
   handleClick(event) {
     const { offsetX: x, offsetY: y } = event.nativeEvent
     const { clientWidth: width, clientHeight: height } = event.target
-    if (event.nativeEvent.offsetX < event.target.clientWidth * RATIO) {
+    if (x < width * RATIO) {
+      const { h, s } = calculateHS(x, y, width * RATIO, height)
+      const v = 0
+      // console.log({ h, s, v })
+      this.props.onChange(`hsv(${h},${s},${v})`)
+    } else {
+      const { h, s } = { h: 0.0, s: 0.0 }
+      const { v } = calculateValueOf(
+        y,
+        (width - x) / width * (1 - RATIO),
+        height,
+        width * (1 - RATIO)
+      )
+      // console.log({ h, s, v })
+      this.props.onChange(`hsv(${h},${s},${v})`)
     }
-    // this.props.onChange(this.props.name, `hsv(${h},${s},${v})`)
+  }
+
+  parsedValue() {
+    const [, h, s, v] =
+      (this.props.value || this.props.defaultValue || '')
+        .match(/hsv\((\d(?:\.\d+)?),(\d(?:\.\d+)?),(\d(?:\.\d+)?)\)/) || []
+    return {
+      h: (h && parseFloat(h)) || 0.0,
+      s: (s && parseFloat(s)) || 0.0,
+      v: (v && parseFloat(v)) || 0.0
+    }
   }
 
   render() {
+    const { h, s, v } = this.parsedValue()
+    const clampedH = Math.floor(h * HUES) / HUES
+    const clampedS = Math.floor((1 - s) * SATURATIONS) / SATURATIONS
+    const clampedV = Math.floor(v * VALUES) / VALUES
     return (
       <div className="parameter color">
         <FormLabel>
           {this.props.label || this.props.name}
         </FormLabel>
-        <canvas
-          id={this.props.name}
-          onClick={this.handleClick}
-          ref={this.renderCanvas}
-          width="600"
-          height="200"
-          style={{ width: '100%', height: 200 }}
-        />
+        <div style={{ position: 'relative' }}>
+          <canvas
+            id={this.props.name}
+            onClick={this.handleClick}
+            ref={this.renderCanvas}
+            width="600"
+            height="200"
+            style={{ width: '100%', height: 200 }}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              left: `calc(${clampedH * 100 * RATIO}% - 2px)`,
+              top: `calc(${clampedS * 100}% - 2px)`,
+              width: `${RATIO / HUES * 100}%`,
+              height: `${1 / SATURATIONS * 100}%`,
+              border: '2px solid black',
+              boxSizing: 'content-box',
+              borderRadius: '2px',
+              pointerEvents: 'none'
+            }}
+          />
+          <div
+            style={{
+              position: 'absolute',
+              left: `calc(${RATIO * 100}% - 2px)`,
+              top: `calc(${clampedV * 100}% - 2px)`,
+              width: `${(1 - RATIO) * 100}%`,
+              height: `${1 / VALUES * 100}%`,
+              border: '2px solid white',
+              boxSizing: 'content-box',
+              borderRadius: '2px',
+              pointerEvents: 'none'
+            }}
+          />
+        </div>
       </div>
     )
   }
@@ -85,11 +154,13 @@ class Color extends React.Component {
 Color.propTypes = {
   name: string.isRequired,
   label: string,
+  value: string,
   defaultValue: string,
   onChange: func.isRequired
 }
 
 Color.defaultProps = {
+  value: '',
   defaultValue: 'hsv(0.0,0.0,0.0)',
   label: ''
 }
